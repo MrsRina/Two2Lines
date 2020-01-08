5f7d8cd8f6d37c2e4289644c82dcb715ab671ba4
Wks JanStudio <37088203+SirRina@users.noreply.github.com>
2020-01-08 13:54:31 -0300

    New things new things!
    Removed jogl and added glfw...
    
    All rights reserved.

--- Data/application.java
+++ Data/application.java
@@ -1,23 +1,45 @@
+// Imports all contents.
+
 import org.lwjgl.system.*;
-import org.lwjgl.opengl.*;
 import org.lwjgl.glfw.*;
 import org.lwjgl.*;
 
 import java.nio.*;
 
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.MemoryUtil.*;
-import static org.lwjgl.glfw.Callbacks.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.glfw.GLFW.*;
 
 class application 
 {
+	private long twoWindow;
+
 	public void main(String[] args) 
 	{
 		int twoWidth  = 1280;
 		int twoHeight = 800;
 
 		String twoTitle = "Two2Lines";
+
+		new application().twoGameProcess();
+	}
+
+	public void twoGameProcess()
+	{
+		glfwInit();
+
+		glfwDefaultWindowHints();
+		glfwWindowHint(GLFW_VISIBLE, GLWF_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GLWF_TRUE);
+
+		twoWindow = glfwCreateWindow(twoWidth, twoHeight, twoTitle, NULL, NULL);
+
+		new application().twoSycnFinshApplication();
+	}
+
+	public void twoSycnFinshApplication()
+	{
+		glfwSetKeyCallBck(twoWindow, (twoWindow, key, scancode, action, mods) -> 
+		{
+			if (key == GLFW_KEY_ESCAPE && action == GLFW_REALSE)
+				glfwSetWindowShouldClose(twoWindow, true);
+		});
 	}
 }
--- run.py
+++ run.py
@@ -8,10 +8,13 @@ class Run_Aplication:
 
 	def execute(self, lib):
 		try:
-			lib.system("javac -classpath {lwjgl} Data/application.java".format(
-			lwjgl = "Libs/lwjgl.jar"
-			)
-			)
+			lib.system("javac -classpath {} Data/application.java".format((
+				"Libs/lwjgl.jar;" +
+				"Libs/lwjgl-glfw.jar;" +
+				"Libs/lwjgl-javadoc.jar;" +
+				"Libs/lwjgl-glfw-natives-windows.jar.jar;" +
+				"Libs/lwjgl-glfw-sources.jar"
+				)))
 		except:
 			raise
 		return None
